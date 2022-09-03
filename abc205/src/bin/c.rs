d78620d0780bb6cb42de8585c82926aca0239e1d
hideyuk1-jp <hh11028+github@gmail.com>
2022-09-03 15:31:19 +0900

    abc205: c

--- abc205/src/bin/c.rs
+++ abc205/src/bin/c.rs
@@ -1,6 +1,19 @@
 use proconio::{fastout, input};
+use std::cmp::Ordering::*;
 
 #[fastout]
 fn main() {
-    input!();
+    input! {
+        (mut a, mut b, c): (i32, i32, u32),
+    };
+    if c % 2 == 0 {
+        a = a.abs();
+        b = b.abs();
+    }
+    let ans = match a.cmp(&b) {
+        Greater => ">",
+        Less => "<",
+        Equal => "=",
+    };
+    println!("{}", ans);
 }
